2a072cf5a224c2b19daa05c01d4052a001faed70
Thanh <thanhvv@gmail.com>
2017-12-27 14:52:11 -0800

    Add intial files.

---
+++ PizzaML/html/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Three Aces pizza house</title>	
+	</head>
+	<body>
+		<h1 text-align=center> <b>Welcome to Three Aces, the greatest pizza house on earth!!</b> </h1>
+	</body>
+</html>
+
