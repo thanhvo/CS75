3db2e0ccf620fb7ed20d4fa2e05f2e2a8e2333a3
Thanh <thanhvv@gmail.com>
2018-02-23 15:15:55 -0800

    Correct the XML format of the menu. Compute the cost of an order by searching the XML file.

--- PizzaML/html/cart.php
+++ PizzaML/html/cart.php
@@ -13,6 +13,30 @@
 		public function print() {
 			echo $this->item . "\t" . $this->size . "\t" . $this->quantity; 
 			echo "<br>";
+			echo "The total cost is: " . $this->cost();
+		}
+		
+		public function cost() {
+				// Parsing XML file of the menu with XMLReader with Expand - DOM/DOMXpath 
+				$reader = new XMLReader();
+ 				$reader->open("../data/menu.xml");
+				$sum = 0;
+				while ($reader->read()) {
+					switch ($reader->nodeType) {
+						case (XMLREADER::ELEMENT):
+							if ($reader->localName == "item" && 
+							$reader->getAttribute("name") == $this->item) {
+								$node = $reader->expand();
+								foreach ($node->childNodes as $child) {									
+									if ($child->hasAttributes() && 
+									$child->attributes["size"]->nodeValue == $this->size) {
+										$sum += $child->nodeValue * $this->quantity;
+									}									
+								}								
+							}
+					}
+				}
+				return $sum;
 		}
 	}
 ?>
